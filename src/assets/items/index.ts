bf05425b970341bd7f204975921f5f87ac9f9602
robertokbr <robertojuniordev@gmail.com>
2021-04-10 21:22:27 -0300

    Add placeholder assets

---
+++ src/assets/items/index.ts
@@ -0,0 +1,19 @@
+import image_one from './1.jpeg';
+import image_two from './2.jpeg';
+import image_three from './3.jpeg';
+import image_four from './4.jpeg';
+import image_five from './5.jpeg';
+import image_six from './6.jpeg';
+import image_seven from './7.jpeg';
+import image_eight from './8.jpeg';
+
+export default [
+  image_one,
+  image_two,
+  image_three,
+  image_four,
+  image_five,
+  image_six,
+  image_seven,
+  image_eight,
+]
