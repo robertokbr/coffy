ad8ba496121293b0e1de1b3e79d33d5aa2337ef4
robertokbr <robertojuniordev@gmail.com>
2021-04-10 21:21:49 -0300

    Update global colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+export default {
+  background: '#f8f8f8',
+  backgroundTwo: '#1a1a1a',
+  backgroundThree: '#312927',
+  primary: '#dd7329' ,
+  black: '#020202',
+  blackTwo: '#a6a6a6',
+  white: '#e6e6e6',
+}
