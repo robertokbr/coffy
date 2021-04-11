705c600b627f44d39f00ed505dcdf270e602f66f
robertokbr <robertojuniordev@gmail.com>
2021-04-10 21:22:14 -0300

    Finish routes

---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Home from '../pages/Home';
+import TabRouter from './tab.routes';
+import colors from '../styles/colors';
+import CreateOrder from '../pages/CreateOrder';
+
+const Stack = createStackNavigator();
+
+const StackRoutes: React.FC = () => (
+  <NavigationContainer>
+    <Stack.Navigator
+      headerMode="none"
+      initialRouteName="Home"
+      screenOptions={{ 
+        cardStyle: {
+          backgroundColor: colors.background,
+        }}
+      }
+    >
+      <Stack.Screen  name="Home" component={Home} />
+      <Stack.Screen  name="CreateOrder" component={CreateOrder} />
+      <Stack.Screen  name="Tabs" component={TabRouter} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default StackRoutes;
---
+++ src/routes/tab.routes.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { View as MotiView } from 'moti';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather } from '@expo/vector-icons';
+
+import colors from '../styles/colors';
+import Gallery from '../pages/Gallery';
+import Orders from '../pages/Orders';
+
+const Tab = createBottomTabNavigator();
+
+const TabBarIconContainer: React.FC<{ focused: boolean }> = ({ 
+  focused,
+  children 
+}) => (
+  <MotiView
+    animate={{
+      scale: focused ? 1 : 0.8, 
+      height: 48, 
+      width: 48,
+    }} 
+    style={{ 
+      borderRadius: 24, 
+      backgroundColor: 
+        focused ? colors.backgroundThree : colors.backgroundTwo,
+      alignItems: 'center',
+      justifyContent: 'center' 
+    }}>
+    {children}
+  </MotiView>
+);
+
+const TabRoutes: React.FC = () => (
+  <Tab.Navigator
+    sceneContainerStyle={{ backgroundColor: colors.background }}
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ color, size, focused }) => {
+        if (route.name === 'Gallery') {
+          return (
+            <TabBarIconContainer focused={focused}>
+              <Feather name="grid" size={size} color={color} />
+            </TabBarIconContainer>
+          )
+        }
+        if (route.name === 'Orders') {
+          return (
+            <TabBarIconContainer focused={focused}>
+              <Feather name="list" size={size} color={color} />
+            </TabBarIconContainer>
+          )
+        }
+      },
+    })}
+    tabBarOptions={{
+      style: {
+        height: 64,
+        backgroundColor: colors.backgroundTwo,
+        borderTopWidth: 0,
+        marginBottom: 16,
+        marginHorizontal: 16,
+        borderRadius: 16,
+      },
+      tabStyle: {
+        alignItems: `center`,
+        justifyContent: `center`,
+      },
+      iconStyle: {
+        width: 20,
+        height: 20,
+        marginBottom: 0,
+      },
+      inactiveTintColor: colors.blackTwo,
+      activeTintColor: colors.primary,
+      showLabel: false
+    }}
+    initialRouteName="Gallery"
+  >
+    <Tab.Screen name="Gallery" component={Gallery} />
+    <Tab.Screen name="Orders" component={Orders} />
+  </Tab.Navigator>
+);
+
+export default TabRoutes;
