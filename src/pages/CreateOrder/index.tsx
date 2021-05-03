ded1366268aee3da63d921e854402d2fb29ad638
robertokbr <robertojuniordev@gmail.com>
2021-05-03 19:04:25 -0300

    Change CreateOrderPage button usability

--- src/pages/CreateOrder/index.tsx
+++ src/pages/CreateOrder/index.tsx
@@ -1,18 +1,19 @@
+/* eslint-disable react/jsx-curly-newline */
 import React, { useCallback, useEffect, useState } from 'react';
 import { Entypo } from '@expo/vector-icons';
 import { Feather } from '@expo/vector-icons';
 
+import { useNavigation } from '@react-navigation/core';
 import * as S from './styles';
 import colors from '../../styles/colors';
-import { useNavigation } from '@react-navigation/core';
 import { useOrder } from '../../hooks/useOrder';
 import Load from '../../components/Load';
-import noContentImg from '../../assets/illustrations/no-food.png'; 
+import noContentImg from '../../assets/illustrations/no-food.png';
 
 const CreateOrder: React.FC = () => {
   const { navigate } = useNavigation();
-  const { 
-    orderProducts, 
+  const {
+    orderProducts,
     handleProductQuantity,
     handleCreateOrder,
   } = useOrder();
@@ -32,12 +33,12 @@ const CreateOrder: React.FC = () => {
       setIsLoading(false);
       navigate('Gallery');
     });
-  }, [handleCreateOrder, navigate]);
+  }, [handleCreateOrder, navigate, orderProducts.length]);
 
   return (
     <>
       <S.Container>
-        <S.OrderList 
+        <S.OrderList
           contentContainerStyle={{
             paddingHorizontal: 24,
           }}
@@ -47,9 +48,9 @@ const CreateOrder: React.FC = () => {
             <S.Header>
               <S.HeaderContent>
                 <S.GoBackButton onPress={handleNavigateBack}>
-                  <Feather 
-                    name="chevron-left" 
-                    size={24} 
+                  <Feather
+                    name="chevron-left"
+                    size={24}
                     color={colors.backgroundThree}
                   />
                 </S.GoBackButton>
@@ -59,50 +60,49 @@ const CreateOrder: React.FC = () => {
           ListEmptyComponent={
             <S.NoContentContainer>
               <S.NoContentImage source={noContentImg} />
-              <S.NoContentTitle>
-                No Food selected
-              </S.NoContentTitle>
+              <S.NoContentTitle>No Food selected</S.NoContentTitle>
             </S.NoContentContainer>
           }
           renderItem={({ item: { product, quantity } }) => (
             <S.Item>
-            <S.ItemImage source={{ uri: product.image_url }} />
-            <S.TitleContainer>
-              <S.Title>
-                {product.name}
-              </S.Title>
-              <S.SubTitle numberOfLines={1}>
-                {product.description}
-              </S.SubTitle>
-            </S.TitleContainer>
-            <S.PickQuantity>
-              <S.MinusQuantity 
-                onPress={() => handleProductQuantity({ product, quantity: -1})}
-              >
-              <Entypo name="minus" size={20} color={colors.blackTwo} />
-              </S.MinusQuantity>
-              <S.QuantityText>
-                {quantity}
-              </S.QuantityText>
-              <S.PlusQuantity 
-                onPress={() => handleProductQuantity({ product, quantity: 1})}
-              >
-              <Entypo name="plus" size={20} color={colors.white} />
-              </S.PlusQuantity>
-            </S.PickQuantity>
-          </S.Item>
+              <S.ItemImage source={{ uri: product.image_url }} />
+              <S.TitleContainer>
+                <S.Title>{product.name}</S.Title>
+                <S.SubTitle numberOfLines={1}>{product.description}</S.SubTitle>
+              </S.TitleContainer>
+              <S.PickQuantity>
+                <S.MinusQuantity
+                  onPress={() =>
+                    handleProductQuantity({ product, quantity: -1 })
+                  }
+                >
+                  <Entypo name="minus" size={20} color={colors.blackTwo} />
+                </S.MinusQuantity>
+                <S.QuantityText>{quantity}</S.QuantityText>
+                <S.PlusQuantity
+                  onPress={() =>
+                    handleProductQuantity({ product, quantity: 1 })
+                  }
+                >
+                  <Entypo name="plus" size={20} color={colors.white} />
+                </S.PlusQuantity>
+              </S.PickQuantity>
+            </S.Item>
           )}
-        >
-        </S.OrderList>
+        />
       </S.Container>
       {isLoading && <Load isShadow />}
       <S.Footer>
-        <S.Button onPress={handleFinishOrder}>
+        <S.Button
+          onPress={
+            !orderProducts.length ? handleNavigateBack : handleFinishOrder
+          }
+        >
           <S.ButtonIcon>
-            <Feather name="arrow-right" size={20} color="#dd7329"/>
+            <Feather name="arrow-right" size={20} color="#dd7329" />
           </S.ButtonIcon>
           <S.ButtonText>
-            Finish Order
+            {!orderProducts.length ? 'Pick products' : 'Finish Order'}
           </S.ButtonText>
         </S.Button>
       </S.Footer>
